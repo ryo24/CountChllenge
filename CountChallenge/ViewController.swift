29563fb88bcb33e09f96df2082516b5079b8cd79
Ryo Yamamoto <ryo24.yamamoto@gmail.com>
2016-01-30 15:18:21 +0900

    Initial Commit

---
+++ CountChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountChallenge
+//
+//  Created by YAMAMOTORYO on 2016/01/30.
+//  Copyright © 2016年 YAMAMOTORYO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
