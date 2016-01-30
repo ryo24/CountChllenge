83ef074b76d80a239d2fb386e4798ea585b99549
Ryo Yamamoto <ryo24.yamamoto@gmail.com>
2016-01-30 15:24:01 +0900

    commit

--- CountChallenge/ViewController.swift
+++ CountChallenge/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func plus(){
+        number++
+        label.text = String(number)
     }
 
 
